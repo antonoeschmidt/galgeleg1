e72f99764dffaf06903b45ab7ac33a1e7587153d
antonoeschmidt <s163053@student.dtu.dk>
2020-03-14 15:19:23 +0100

    Ændret klient

--- RMI/src/main/java/galgeleg/Galgeklient.java
+++ RMI/src/main/java/galgeleg/Galgeklient.java
@@ -1,6 +1,8 @@
 package galgeleg;
 
+import java.net.MalformedURLException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.Scanner;
 
@@ -15,19 +17,25 @@ import brugerautorisation.transport.Brugeradmin;
 public class Galgeklient {
     public static void main(String[] args) throws Exception {
 
-        GalgelegInterface glI = (GalgelegInterface) Naming.lookup("rmi://localhost:1099/galgeservice");
-        //GalgelegInterface glI =(GalgelegInterface) Naming.lookup("rmi://dist.saluton.dk:23609/kontotjeneste");
-        glI.nulstil();
-        Scanner scan = new Scanner(System.in);
+        brugerAut();
 
-        //Brugeradmin brugeradmin = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
-        System.out.println("Indtast studienummer: ");
-        String studienummer = scan.nextLine();
-        System.out.println("Indtast kode: ");
-        String kode = scan.nextLine();
-        if (glI.login(studienummer, kode)) {
-            System.out.println("suck");
-        }
+//        GalgelegInterface glI = (GalgelegInterface) Naming.lookup("rmi://localhost:1099/galgeservice");
+//        //GalgelegInterface glI =(GalgelegInterface) Naming.lookup("rmi://dist.saluton.dk:23699/kontotjeneste");
+//        glI.nulstil();
+//        Scanner scan = new Scanner(System.in);
+//
+//        Brugeradmin brugeradmin = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+//        System.out.println("Indtast studienummer: ");
+//        String studienummer = scan.nextLine();
+//        System.out.println("Indtast kode: ");
+//        String kode = scan.nextLine();
+//
+//        brugeradmin.hentBruger(studienummer,kode);
+//
+//
+//        if (glI.login(studienummer, kode)) {
+//            System.out.println("suck");
+//        }
         /*
         try {
             brugeradmin.hentBruger(studienummer,kode);
@@ -53,8 +61,18 @@ public class Galgeklient {
             System.out.println(glI.getSynligtOrd());
             System.out.println(7 - glI.getAntalForkerteBogstaver() + " liv tilbage");
         }
-
         System.out.println(glI.erSpilletVundet() ? "Tillykke du vandt!" : "Du tabte desværre. Ordet du prøvede at gætte var: " + glI.getOrdet());
+    }
+
+    public static void brugerAut() throws RemoteException, NotBoundException, MalformedURLException {
+        Brugeradmin brugeradmin = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Indtast studienummer: ");
+        String studienummer = scan.nextLine();
+        System.out.println("Indtast kode: ");
+        String kode = scan.nextLine();
+        brugeradmin.hentBruger(studienummer,kode);
+
     }
 
 
